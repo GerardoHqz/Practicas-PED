34cc36b0de299138a79f0d3a7f2ab1c519abf892
GerardoHqz <gerardohenriquez.sv@gmail.com>
2020-11-23 22:47:23 -0600

    Arboles

--- Guia#6/Ejercicio1.cpp
+++ Guia#6/Ejercicio1.cpp
@@ -18,36 +18,41 @@ void PostOrder(ABB);
 int AlturArbol(ABB);
 int CalcularNodos(ABB);
 int SumaDeNodos(ABB);
+int NodosPares(ABB);
+int NodosImpares(ABB);
+int SumaPares(ABB);
+int SumaImpares(ABB);
 
 int main(){
     ABB arbol;
     bool status = true,status2 = true;
     int opcion,opcion2,raiz,dato;
 
+    cout<<"\nIngrese la riaz del nodo: "; cin>>raiz;
+    arbol = CrearNodo(raiz);
+
     do{
         system("cls");
         cout<<"\n*****MENU*****\n";
-        cout << "1. Crear arbol\n";
-        cout << "2. Ingresar nodo al arbol\n";
-        cout << "3. Mostrar árbol\n";
-        cout << "4. Mostrar la cantidad de niveles del árbol\n";
-        cout << "5. Mostrar la cantidad de nodos del árbol\n";
-        cout << "6. Mostrar la suma de los valores de todos los nodos del árbol\n";
-        cout << "7. Salir del programa\n";
-        cout << "Ingrese una opcion: "; cin>>opcion;
+        cout << "1. Ingresar nodo al arbol\n";
+        cout << "2. Mostrar árbol\n";
+        cout << "3. Mostrar la cantidad de niveles del árbol\n";
+        cout << "4. Mostrar la cantidad de nodos del árbol\n";
+        cout << "5. Mostrar la suma de los valores de todos los nodos del árbol\n";
+        cout << "6. Mostrar la cantidad de nodos pares en el arbol\n";
+        cout << "7. Mostrar la cantidad de nodos impares en al arbol\n";
+        cout << "8. Mostrar la suma de los datos pares del arbol\n";
+        cout << "9. Mostrar la suma de los datos impares del arbol\n";
+        cout << "10. Salir del programa\n";
+        cout << "\nIngrese una opcion: "; cin>>opcion;
 
         switch(opcion){
             case 1:
-                cout<<"\nIngrese la riaz del nodo: "; cin>>raiz;
-                CrearNodo(raiz);
-                system("pause");
-                break;
-            case 2:
                 cout<<"\nIngrese un dato: "; cin>>dato;
                 AsignarNodo(arbol,dato);
                 system("pause");
                 break;
-            case 3:
+            case 2:
                 do{
                     cout<<"\nFORMAS DE MOSTRAR EL ARBOL\n";
                     cout<<"1. Pre orden\n";
@@ -78,19 +83,35 @@ int main(){
                     }
                 }while(status2); 
                 break;
+            case 3:
+                cout<<"\nEl arbol tiene una altura de "<<AlturArbol(arbol)<<" nodos"<<endl;
+                system("pause");
+                break;
             case 4:
-                cout<<"\nEl arbol tiene una altura de "<<AlturArbol(arbol)<<" nodos";
+                cout<<"\nEl arbol tiene: "<<CalcularNodos(arbol)<<" nodos"<<endl;
                 system("pause");
                 break;
             case 5:
-                cout<<"\nEl arbol tiene: "<<CalcularNodos<<" nodos";
+                cout<<"La suma de los datos de los nodos es: "<<SumaDeNodos(arbol)<<" nodos"<<endl;
                 system("pause");
                 break;
             case 6:
-                cout<<"La suma de los datos de los nodos es: "<<SumaDeNodos(arbol)<<" nodos";
+                cout<<"La cantidad de nodos pares son "<<NodosPares(arbol)<<" nodos"<<endl;
                 system("pause");
                 break;
             case 7:
+                cout<<"La cantidad de nodos impares son "<<NodosImpares(arbol)<<" nodos"<<endl;
+                system("pause");
+                break;
+            case 8:
+                cout<<"La suma de los datos pares es "<<SumaPares(arbol)<<endl;
+                system("pause");
+                break;
+            case 9:
+                cout<<"La suma de los datos impares son "<<SumaImpares(arbol)<<endl;
+                system("pause");
+                break;
+            case 10:
                 status = false;
                 cout<<"\nFin del programa\n";
                 system("pause");
@@ -128,7 +149,6 @@ void AsignarNodo(ABB arbol,int valor){
             }
             else{
                 temporal = temporal->izq;
-                break;
             }
         }
         else{
@@ -213,5 +233,77 @@ int SumaDeNodos(ABB arbol){
     }
     else{
         return 0;
+    }
+}
+
+int NodosPares(ABB arbol){
+    int pares = 0;
+    if( arbol != NULL){
+        if(arbol->dato%2 == 0){
+            pares++;
+        }
+        if(arbol->izq != NULL){
+            pares += NodosPares(arbol->izq);
+        }
+        if(arbol->der != NULL){
+            pares += NodosPares(arbol->der);
+        }
+        return pares;
+    }
+    else{
+        return 0;
+    }
+}
+
+int NodosImpares(ABB arbol){
+    int impares = 0;
+    if(arbol != NULL){
+        if(arbol->dato%2 != 0){
+            impares++;
+        }
+        if(arbol->izq != NULL){
+            impares += NodosImpares(arbol->izq);
+        }
+        if(arbol->der != NULL){
+            impares += NodosImpares(arbol->der);
+        }
+        return impares;
+    }
+    else{
+        return 0;
+    }
+}
+
+int SumaPares(ABB arbol){
+    int suma = 0;
+    if(arbol != NULL){
+        if(arbol->dato%2 == 0){
+            suma += arbol->dato;
+        }
+        if(arbol->izq != NULL){
+            suma += SumaPares(arbol->izq);
+        }
+        if(arbol->der != NULL){
+            suma += SumaPares(arbol->der);
+        }
+        return suma;
+    }
+    else{
+        return 0;
+    }
+}
+
+int SumaImpares(ABB arbol){
+    int impares = 0;
+    if(arbol != NULL){
+        if(arbol->dato%2 != 0){
+            impares += arbol->dato;
+        }
+        if(arbol->izq != NULL){
+            impares += SumaImpares(arbol->izq);
+        }
+        if(arbol->der != NULL){
+            impares += SumaImpares(arbol->der);
+        }
     }
 }
